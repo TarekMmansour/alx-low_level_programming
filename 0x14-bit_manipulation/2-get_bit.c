2f01a9054767d2184f0024c6adafe20061373cd1
TarekMmansour <124052844+TarekMmansour@users.noreply.github.com>
2023-04-10 05:44:03 +0200

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,14 +1,7 @@
-#include "holberton.h"
+
 #include <limits.h>
 #include <stdio.h>
 
-/**
- * get_bit - function that returns the value of a bit at a given index.
- * @n: number to scan.
- * @index: bit to return.
- *
- * Return: Bit (0 | 1)
- */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit;
